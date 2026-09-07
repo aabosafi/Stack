222e3787899ad5f1eb8a6026157ebc4638a89c21
aabosafi <aabosafi@gmail.com>
2026-09-06 17:30:37 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cache-first shell so the tracker opens with no signal. */
-const CACHE = 'stack-v5';
+const CACHE = 'stack-v6';
 const ASSETS = ['./', './index.html', './guide.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
